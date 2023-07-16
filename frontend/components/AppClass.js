42451886bd49e2125fdf1918dcd2b602080885cf
Frankie <frankiejosefej@gmail.com>
2023-07-15 23:37:39 -0400

    met mvp1

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -6,80 +6,215 @@ const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
-
 export default class AppClass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        message: initialMessage,
+        email: initialEmail,
+        index: initialIndex,
+        steps: initialSteps,
+    }
+  }
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    const gridSize = 3;
+    const index = this.state.index;
+
+    const x = (index % gridSize);
+    const y = (Math.floor(index / gridSize));
+
+    return { x, y }
   }
 
   getXYMessage = () => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    const { x, y } = this.getXY();
+
+    return `Coordinates (${x + 1}, ${y + 1})`;
   }
 
-  reset = () => {
-    // Use this helper to reset all states to their initial values.
+ reset = () => {
+  // Use this helper to reset all states to their initial values.
+  this.setState({
+    message: initialMessage,
+    email: initialEmail,
+    index: initialIndex,
+    steps: initialSteps,
+  });
+};
+
+
+getNextIndex = (direction) => {
+  const gridSize = 3; // Assuming the grid size is 3x3, change this if your grid is different
+  const currentIndex = this.state.index;
+  let nextIndex;
+
+  switch (direction) {
+    case 'left':
+      nextIndex = currentIndex - 1;
+      break;
+    case 'right':
+      nextIndex = currentIndex + 1;
+      break;
+    case 'up':
+      nextIndex = currentIndex - gridSize;
+      break;
+    case 'down':
+      nextIndex = currentIndex + gridSize;
+      break;
+    default:
+      nextIndex = currentIndex;
+      break;
   }
 
-  getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+  // Check if the nextIndex is out of bounds, and if it is, keep the current index unchanged
+  if (
+    nextIndex < 0 || // Check for left and top edges
+    nextIndex >= gridSize * gridSize || // Check for right and bottom edges
+    (direction === 'left' && nextIndex % gridSize === gridSize - 1) || // Check for wrapping from left to right
+    (direction === 'right' && nextIndex % gridSize === 0) // Check for wrapping from right to left
+  ) {
+    return currentIndex;
   }
 
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+  return nextIndex;
+};
+
+move = (evt) => {
+  const direction = evt.target.id;
+  const currentIndex = this.state.index;
+  const newIndex = this.getNextIndex(direction);
+
+  // Check if the new index is the same as the current index
+  // If they are the same, "B" didn't move, so show an error message
+  if (currentIndex === newIndex) {
+    const errorMessage = `You can't go ${direction}`;
+    this.setState({ message: errorMessage });
+  } else {
+    // If the move is possible, reset the error message and update the state
+    this.setState((prevState) => ({
+      index: newIndex,
+      steps: prevState.steps + 1,
+      message: '' // Reset the error message
+    }));
   }
+};
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
+    const newValue = evt.target.value;
+    this.setState({
+      [evt.target.id]: newValue,
+    });
   }
 
+  
   onSubmit = (evt) => {
-    // Use a POST request to send a payload to the server.
-  }
+    evt.preventDefault();
+    this.setState({ message: '' });
+
+    const { x, y } = this.getXY();
+    const steps = this.state.steps;
+    const email = this.state.email;
+
+    if (email === 'foo@bar.baz') {
+      this.setState({ message: `${email} failure #71`, email: '', index: initialIndex, steps: initialSteps });
+      return;
+    }
+
+    if (!this.isValidEmail(email)) {
+      this.setState({ message:  "Ouch: email is required" + ' Ouch: email must be a valid email' });
+      return;
+    }
+
+    // Adjust x and y coordinates to 1-based indexing
+    const adjustedPayload = {
+      x: x + 1,
+      y: y + 1,
+      steps,
+      email,
+    };
+
+    fetch('http://localhost:9000/api/result', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(adjustedPayload),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 422) {
+            throw new Error('Unprocessable Entity: Payload has the wrong shape.');
+          } else {
+            throw new Error('Network response was not ok.');
+          }
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Reset the coordinates to (3, 1) after successful form submission
+        this.setState(() => ({
+          message: data.message,
+          index: 2, // Set the index to 2 for coordinates (3, 1)
+          steps: 2,
+          email: '',
+        }));
+      })
+      .catch((error) => {
+        console.error('Error occurred:', error);
+        if (error.message.includes('foo@bar.baz')) {
+          this.setState({ message: `foo@bar.baz failure #71`, index: 2, steps: initialSteps, email: '' });
+        }
+      });
+  };
+
+  // Helper function to check if the email is valid
+  isValidEmail = (email) => {
+    // Basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  
 
   render() {
-    const { className } = this.props
+    const { className } = this.props;
+    const {message } = this.state;
+
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{this.getXYMessage()}</h3>
+          <h3 id="steps">You moved {this.state.steps} {this.state.steps === 1 ? 'time' : 'times'}</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.index   ? ' active' : ''}`}>
+                {idx === this.state.index ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.move}>LEFT</button>
+          <button id="up" onClick={this.move}>UP</button>
+          <button id="right" onClick={this.move}>RIGHT</button>
+          <button id="down" onClick={this.move}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" placeholder="type email" onChange={this.onChange} value={this.state.email}></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,224 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialMessage = '';
+const initialEmail = '';
+const initialSteps = 0;
+const initialIndex = 4; // the index the "B" is at
 
-export default function AppFunctional(props) {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+const AppFunctional = ({ className }) => {
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [index, setIndex] = useState(initialIndex);
+  const [steps, setSteps] = useState(initialSteps);
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+  // Helper function to get coordinates based on the current index
+  const getXY = () => {
+    const gridSize = 3;
+    const x = index % gridSize;
+    const y = Math.floor(index / gridSize);
+    return { x, y };
+  };
+
+  // Helper function to get the message for the current coordinates
+  const getXYMessage = () => {
+    const { x, y } = getXY();
+    return `Coordinates (${x + 1}, ${y + 1})`;
+  };
+
+  // Helper function to check if the email is valid
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  // Helper function to calculate the next index based on the direction
+  const getNextIndex = (direction) => {
+    const gridSize = 3;
+    let nextIndex;
+
+    switch (direction) {
+      case 'left':
+        nextIndex = index - 1;
+        break;
+      case 'right':
+        nextIndex = index + 1;
+        break;
+      case 'up':
+        nextIndex = index - gridSize;
+        break;
+      case 'down':
+        nextIndex = index + gridSize;
+        break;
+      default:
+        nextIndex = index;
+        break;
+    }
+
+    if (
+      nextIndex < 0 ||
+      nextIndex >= gridSize * gridSize ||
+      (direction === 'left' && nextIndex % gridSize === gridSize - 1) ||
+      (direction === 'right' && nextIndex % gridSize === 0)
+    ) {
+      return index;
+    }
+
+    return nextIndex;
+  };
+
+  // Function to handle the movement
+  const move = (evt) => {
+    const direction = evt.target.id;
+    const newIndex = getNextIndex(direction);
+
+    if (index === newIndex) {
+      // B didn't move, show an error message
+      setMessage(`You can't go ${direction}`);
+    } else {
+      // If the move is possible, reset the error message and update the state
+      setIndex(newIndex);
+      setSteps((prevSteps) => prevSteps + 1);
+      setMessage('');
+    }
+  };
+
+  // Function to handle form submission
+  // Function to handle form submission
+// Function to handle form submission
+const onSubmit = (evt) => {
+  evt.preventDefault();
+  setMessage('');
+  const { x, y } = getXY();
+  const payload = {
+    x: x + 1, // Add 1 to x and y to match the server's 1-based index requirement
+    y: y + 1,
+    steps,
+    email,
+  };
+
+  if (email === 'foo@bar.baz') {
+    setMessage(`${email} failure #71`);
+    setEmail('');
+    setIndex(initialIndex);
+    setSteps(initialSteps);
+    return;
   }
 
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  if (!isValidEmail(email)) {
+    setMessage('Ouch: email is required' + ' Ouch: email must be a valid email');
+    return;
   }
 
-  function reset() {
-    // Use this helper to reset all states to their initial values.
+  if (!Number.isInteger(payload.x) || payload.x < 1 || payload.x > 3) {
+    setMessage('Invalid x value. It should be an integer between 1 and 3.');
+    return;
   }
 
-  function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+  if (!Number.isInteger(payload.y) || payload.y < 1 || payload.y > 3) {
+    setMessage('Invalid y value. It should be an integer between 1 and 3.');
+    return;
   }
 
-  function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+  if (!Number.isInteger(payload.steps) || payload.steps <= 0) {
+    setMessage('Invalid steps value. It should be an integer larger than 0.');
+    return;
   }
 
-  function onChange(evt) {
-    // You will need this to update the value of the input.
+  // Perform additional email validation if needed
+  if (!isValidEmail(payload.email)) {
+    setMessage('Ouch: email must be a valid email');
+    return;
   }
 
-  function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
-  }
+  // Update the steps before the fetch request
+  setSteps((prevSteps) => prevSteps       );
+
+  // Continue with the fetch request
+  fetch('http://localhost:9000/api/result', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        if (response.status === 422) {
+          throw new Error('Unprocessable Entity: Payload has the wrong shape.');
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Reset the coordinates after a successful response
+      setIndex(2);
+      setMessage(data.message);
+      setEmail('');
+    })
+    .catch((error) => {
+      console.error('Error occurred:', error);
+      if (error.message.includes('foo@bar.baz')) {
+        setMessage(`foo@bar.baz failure #71`);
+        setIndex(2);
+        setEmail('');
+      }
+    });
+};
+
+  const onChange = (evt) => {
+    const newValue = evt.target.value;
+    setEmail(newValue);
+  };
+
+  const reset = () => {
+    setEmail(initialEmail);
+    setIndex(initialIndex);
+    setSteps(initialSteps);
+    setMessage(initialMessage);
+  };
 
   return (
-    <div id="wrapper" className={props.className}>
+    <div id="wrapper" className={className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{getXYMessage()}</h3>
+        <h3 id="steps">You moved {steps} {steps === 1 ? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+            {idx === index ? 'B' : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={move}>
+          LEFT
+        </button>
+        <button id="up" onClick={move}>
+          UP
+        </button>
+        <button id="right" onClick={move}>
+          RIGHT
+        </button>
+        <button id="down" onClick={move}>
+          DOWN
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
-}
+  );
+};
+
+export default AppFunctional;
